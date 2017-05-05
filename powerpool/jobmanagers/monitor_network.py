2126dbe759bc00ab22db03521567ce3715f0cb55
Cory Fields <cory-nospam-@coryfields.com>
2017-05-05 16:26:46 -0400

    fix broken desc_string
    Has this really been broken since 2015?

--- powerpool/jobmanagers/monitor_network.py
+++ powerpool/jobmanagers/monitor_network.py
@@ -359,8 +359,7 @@ class MonitorNetwork(Jobmanager, NodeMonitorMixin):
         coinbase.inputs.append(
             Input.coinbase(self._last_gbt['height'],
                            addtl_push=[mm_data] if mm_data else [],
-                           extra_script_sig=b'\0' * extranonce_length,
-                           desc_string=self.config['coinbase_string']))
+                           extra_script_sig=b'\0' * extranonce_length))
 
         coinbase_value = self._last_gbt['coinbasevalue']
 
